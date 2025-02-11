efde868b8af8f7e24809b04206e24b836c139d97
Uriel Rodríguez <162546336+urielrs@users.noreply.github.com>
2025-02-10 22:49:25 -0600

    Create primerosPasos.py

---
+++ primerosPasos.py
@@ -0,0 +1,8 @@
+# Ejemplo de cabecera 
+n = 0
+while n < 10:
+    if(n % 2)==0: # if (si) de condicion
+        print(n, "Es un numero par")
+    else:
+        print(n, "Es un numero impar")
+    n = n+1
